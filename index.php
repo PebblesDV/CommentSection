0259889401d153cf9a5fe17d7a657d9065831c47
PebblesDV <pebblesdevries@gmail.com>
2023-11-16 18:34:30 +0100

    Finished project

--- index.php
+++ index.php
@@ -1,47 +1,6 @@
-<?php
-    
-    function save_data($nam, $eml, $com) {
-
-        include 'connection.php';
-
-        $sql = $conn->prepare("INSERT INTO Comments (name, email, comment) VALUES (?, ?, ?)");
-        $sql->bind_param("sss", $nam, $eml, $com);
-
-        if ($sql->execute()) {
-            echo "New record created succesfully!";
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-
-        $sql->close();
-        $conn->close();
-    }
-
-    $name = "";
-    $email = "";
-    $comment = "";
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $name = check_input($_POST["name"]);
-        $email = check_input($_POST["email"]);
-        $comment = check_input($_POST["comment"]);
-
-        if (empty($name) || empty($email)) {
-            echo "Name and Email are required";
-        } elseif (empty($comment)) {
-            echo "Comment is required";
-        } else {
-            save_data($name, $email, $comment);
-        }
-    }
-
-    function check_input($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
-    ?>
+<?php 
+    include 'addcomment.php';
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -107,7 +66,6 @@
      }
 
      $conn->close();
-    
     ?>
 </body>
 </html>
