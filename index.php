7ad44411c225c012553db16972531d7fbf84716a
PebblesDV <pebblesdevries@gmail.com>
2023-11-16 18:06:57 +0100

    Placed comment result code in index.php

--- index.php
+++ index.php
@@ -1,3 +1,48 @@
+<?php
+    
+    function save_data($nam, $eml, $com) {
+
+        include 'connection.php';
+
+        $sql = $conn->prepare("INSERT INTO Comments (name, email, comment) VALUES (?, ?, ?)");
+        $sql->bind_param("sss", $nam, $eml, $com);
+
+        if ($sql->execute()) {
+            echo "New record created succesfully!";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+        $sql->close();
+        $conn->close();
+    }
+
+    $name = "";
+    $email = "";
+    $comment = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = check_input($_POST["name"]);
+        $email = check_input($_POST["email"]);
+        $comment = check_input($_POST["comment"]);
+
+        if (empty($name) || empty($email)) {
+            echo "Name and Email are required";
+        } elseif (empty($comment)) {
+            echo "Comment is required";
+        } else {
+            save_data($name, $email, $comment);
+        }
+    }
+
+    function check_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,7 +66,7 @@
     <div class="full-form">
     <h1 class="comment-header">Add a comment:</h1>
 
-    <form action="commentresult.php" method="POST">
+    <form action="index.php" method="POST">
         <div class="input-fields">
           <div class="name-input">
             Name: <input type="text" name="name" value="" size="70">
