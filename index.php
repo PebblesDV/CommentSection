e01fc0f4726025d366d07b212f63751dc87ee4aa
PebblesDV <pebblesdevries@gmail.com>
2023-11-16 17:41:29 +0100

    Showing comments

--- index.php
+++ index.php
@@ -7,31 +7,35 @@
     <link rel="stylesheet" href="main.css">
 </head>
 <body>
-    <iframe 
-    width="560" 
-    height="315" 
-    src="https://www.youtube.com/embed/1UA66zOA4GQ?si=lgMLT8Nh5uZJYk9B" 
-    title="YouTube video player" 
-    frameborder="0" 
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-    allowfullscreen></iframe>
+    <div class="youtube-video">
+        <iframe 
+        width="1120" 
+        height="630" 
+        src="https://www.youtube.com/embed/1UA66zOA4GQ?si=lgMLT8Nh5uZJYk9B" 
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        allowfullscreen></iframe>
+        </div>
 
+    <div class="full-form">
     <h1 class="comment-header">Add a comment:</h1>
 
     <form action="commentresult.php" method="POST">
         <div class="input-fields">
           <div class="name-input">
-            Name: <input type="text" name="name" value="" size="30">
+            Name: <input type="text" name="name" value="" size="70">
           </div>
           <div>
-            Email: <input type="text" name="email" value="" size="30">
+            Email: <input type="text" name="email" value="" size="70">
           </div>
         </div>
         <div>
-            <textarea class="commentarea" name="comment" cols="80" rows="10"></textarea>
+            <textarea class="commentarea" name="comment" cols="165" rows="10"></textarea>
         </div>
         <input type="submit" value="Send">
     </form>
+    </div>
 
     <?php
 
@@ -43,9 +47,12 @@
 
      if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
-            echo "<div>";
-            echo "<h1>" . $row["name"] . "</h1>";
-            echo "<p>" . $row["created_at"] . "</p>"
+            echo "<div class='entire-comment'>";
+
+            echo "<div class='name-and-date'>";
+            echo "<h2 class='comment-name'>" . $row["name"] . "</h2>";
+            echo "<p>" . $row["created_at"] . "</p>";
+            echo "</div>";
 
             echo "<p>" . $row["comment"] . "</p>";
             echo "</div>";
